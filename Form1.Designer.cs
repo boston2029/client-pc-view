9406ae1fe18b2b5fd946038576ea6ec75d01ef23
boston2029 <69646049+boston2029@users.noreply.github.com>
2020-12-09 17:09:26 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace client_remote_desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnShare = new System.Windows.Forms.Button();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textIP = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(25, 185);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(110, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnShare
+            // 
+            this.btnShare.Location = new System.Drawing.Point(181, 185);
+            this.btnShare.Name = "btnShare";
+            this.btnShare.Size = new System.Drawing.Size(121, 23);
+            this.btnShare.TabIndex = 1;
+            this.btnShare.Text = "Share My Screen";
+            this.btnShare.UseVisualStyleBackColor = true;
+            this.btnShare.Click += new System.EventHandler(this.btnShare_Click);
+            // 
+            // textPort
+            // 
+            this.textPort.Location = new System.Drawing.Point(109, 130);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(100, 20);
+            this.textPort.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP Address";
+            // 
+            // textIP
+            // 
+            this.textIP.Location = new System.Drawing.Point(109, 75);
+            this.textIP.Name = "textIP";
+            this.textIP.Size = new System.Drawing.Size(100, 20);
+            this.textIP.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(314, 236);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.btnShare);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnShare;
+        private System.Windows.Forms.TextBox textPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textIP;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Sockets;
+using System.Drawing.Imaging;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace client_remote_desktop
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly TcpClient client = new TcpClient();
+        private NetworkStream mainStream;
+        private int portNumber;
+
+
+        private static Image GrabDesktop()
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(screenshot);
+            graphics.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+
+            return screenshot;
+        }
+
+        private void SendDesktopImage()
+        {
+            BinaryFormatter binFormatter = new BinaryFormatter();
+            mainStream = client.GetStream();
+            binFormatter.Serialize(mainStream, GrabDesktop());
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnShare.Enabled = false;
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            portNumber = int.Parse(textPort.Text);
+            try
+            {
+                client.Connect(textIP.Text, portNumber);
+                btnConnect.Text = "Connected";
+                MessageBox.Show("Connected");
+                btnConnect.Enabled = false;
+                btnShare.Enabled = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to connect. Try again.");
+                btnConnect.Text = "Not Connected";
+
+            }
+        }
+
+        private void btnShare_Click(object sender, EventArgs e)
+        {
+            if (btnShare.Text.StartsWith("Share"))
+            {
+                timer1.Start();
+                btnShare.Text = "Stop Sharing";
+
+            }
+            else
+            {
+                timer1.Stop();
+                btnShare.Text = "Share My Screen";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendDesktopImage();
+        }
+    }
+}
